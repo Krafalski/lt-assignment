35cb431622fd1a0ff428f552a5cc5fdc9139e69f
K. Rafalski <kr2174@caa.columbia.edu>
2023-08-30 20:37:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Incomplete functionality
+// Only used to create sample screenshots
+
+// const modal = document.querySelector(".modal");
+// const content = document.querySelector(".modal > div");
+// const articles = document.querySelectorAll("article");
+
+// modal.addEventListener("click", (event) => {
+//   console.log("modal", event.target.classList);
+//   if (event.target.classList[0] === "modal") {
+//     modal.style.display = "none";
+//   }
+// });
+
+// for (let article of articles) {
+//   article.addEventListener("click", (event) => {
+//     modal.style.display = "flex";
+//   });
+// }
